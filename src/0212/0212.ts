65e5b23bea3aaf30614c3f170f3048387e6c4cfe
Michelle <michelle.en4@gmail.com>
2023-02-14 13:27:32 -0500

    pattern randomness

--- src/0212/0212.ts
+++ src/0212/0212.ts
@@ -16,10 +16,11 @@ new p5((p: p5) => {
     p.draw = function () {
         p.background(230)
         p.stroke(0)
+        p.strokeWeight(0.5)
         p.noFill()
         p.translate(p.width / 2, p.height / 2)
 
-        drawings(4)
+        drawings(p.random([1, 2, 3, 4, 5]))
     }
 
     p.mouseClicked = function (e: Event) {
@@ -29,47 +30,59 @@ new p5((p: p5) => {
     }
 
     function drawings(style) {
+        let r_radius = 0.2
+        let r_yi = 0.1
+        let r_xi = p.random(100, 300)
+
         switch (style) {
             case 1:
+                r_radius = p.random(0.01, 0.09)
+                r_yi = p.random(0.007, 0.01)
                 for (let x = -200; x <= 200; x += 20) {
-                    let ybase = -0.00001 * x * x * x
-                    let y = ybase
+                    let ybase = -0.00003 * p.pow(x / 2 + 100, 3)
 
-                    y = ybase
-                    let dec = 4
-                    while (y > -200) {
-                        p.circle(x, y * p.sin(x * 0.01), 10)
-                        y -= dec
-                        dec += 5
+                    for (let yi = -200; yi <= 200; yi += 10) {
+                        let y = ybase * p.sin(yi * r_yi)
+                        p.circle(x, y, p.sin(yi * r_radius) * 5 + 5)
                     }
                 }
+                p.noStroke()
+                p.fill(0)
+                p.textAlign(p.RIGHT)
+                p.text(`r_radius: ${r_radius}`, p.width / 2 - 10, p.height / 2 - 40)
+                p.text(`r_yi: ${r_yi}`, p.width / 2 - 10, p.height / 2 - 25)
                 break
 
             case 2:
-                for (let x = -200; x <= 200; x += 20) {
-                    let ybase = -0.00001 * x * x * x
+                r_radius = p.random(0.05, 0.3)
+                for (let x = -200; x <= 200; x += 10) {
+                    let ybase = -0.00001 * x * (x - r_xi) * (x + r_xi)
                     let y = ybase
                     let inc = 2
                     while (y < 200) {
-                        p.circle(x, y, 10)
+                        p.circle(x, y, p.sin(inc * r_radius) * 5 + 5)
                         y += inc
-                        inc += 2
+                        inc += 1
                     }
 
                     y = ybase
                     let dec = 4
                     while (y > -200) {
-                        p.circle(x, y, 10)
+                        p.circle(x, y, p.sin(inc * r_radius) * 5 + 5)
                         y -= dec
-                        dec += 2
+                        dec += 1
                     }
                 }
+
+                p.noStroke()
+                p.fill(0)
+                p.textAlign(p.RIGHT)
+                p.text(`r_radius: ${r_radius}`, p.width / 2 - 10, p.height / 2 - 40)
+                p.text(`r_xi: ${r_xi}`, p.width / 2 - 10, p.height / 2 - 25)
                 break
             case 3:
                 p.strokeWeight(0.5)
 
-                let r_radius = 0.2
-                let r_yi = 0.1
                 r_radius = p.random(0.1, 0.5)
                 r_yi = p.random(0.05, 0.2)
 
@@ -89,18 +102,39 @@ new p5((p: p5) => {
 
             case 4:
                 p.strokeWeight(0.5)
-
+                r_radius = p.random(0.1, 0.5)
                 for (let x = -200; x <= 200; x += 5) {
-                    let xm = x * 0.05
-                    let ybase = ((3 * p.sin(xm)) / (xm + 1)) * 30
+                    // let ybase = ((3 * p.sin(x)) / x) * 0.02
+                    let ybase = x * x * x * 0.00001
 
                     for (let yi = 0; yi < 20; yi++) {
-                        let y = ybase * p.sin(yi * x * 0.001)
-                        p.circle(x, y, 5)
+                        let y = ybase * p.cos(yi * 0.1) * 3
+                        p.circle(x, y, p.sin(yi * r_radius) * 5 + 5)
                     }
                 }
                 break
 
+            case 5:
+                p.strokeWeight(0.5)
+                r_radius = p.random(0.1, 0.5)
+                r_yi = p.random(0.03, 0.1)
+                for (let x = -200; x <= 200; x += 5) {
+                    // let ybase = ((3 * p.sin(x)) / x) * 0.02
+
+                    let ybase = (x - r_xi / 2) * (x - (r_xi - 150)) * (x + (r_xi - 100)) * 0.00001
+
+                    for (let yi = 0; yi < 20; yi++) {
+                        let y = ybase * p.cos(yi * r_yi) * 3
+                        p.circle(x, y, p.sin(yi * r_radius - 2) * 5 + 5)
+                    }
+                }
+
+                p.noStroke()
+                p.fill(0)
+                p.textAlign(p.RIGHT)
+                p.text(`r_radius: ${r_radius}`, p.width / 2 - 10, p.height / 2 - 40)
+                p.text(`r_xi: ${r_xi}`, p.width / 2 - 10, p.height / 2 - 25)
+                break
             default:
                 break
         }
