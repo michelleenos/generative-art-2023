a1ce39b6f563502ba06d33281a98e7170d262cd6
Michelle Enos <32843306+crankysparrow@users.noreply.github.com>
2023-11-21 20:55:07 -0500

    wiggly paths

--- src/1118/1118.ts
+++ src/1118/1118.ts
@@ -6,7 +6,6 @@ import loop from '~/helpers/loop'
 
 let yellow = '#f6b02c'
 let blue = '#5bc0eb'
-let yellow2 = '#fde74c'
 let green = '#9bc53d'
 let blue2 = '#2c497f'
 let red = '#e55934'
@@ -23,7 +22,7 @@ const colorCombos = [
     [red, yellow],
 ]
 
-let { ctx, resizeCanvas, canvas } = createCanvas(width, height)
+let { ctx, resizeCanvas } = createCanvas(width, height)
 
 class Grid<T = any> {
     items: T[] = []
@@ -37,19 +36,21 @@ class Grid<T = any> {
     }
 
     get(x: number, y: number) {
-        if (x > this.cols - 1 || x < 0 || y > this.rows - 1 || y < 0) {
-            // showMessage(`get out of bounds: ${x}, ${y}`)
-            return undefined
-        }
+        if (!this.xInRange(x) || !this.yInRange(y)) return undefined
         return this.items[x + y * this.cols]
     }
 
     set(x: number, y: number, value: T) {
-        if (x > this.cols - 1 || x < 0 || y > this.rows - 1 || y < 0) {
-            // showMessage(`set out of bounds: ${x}, ${y}`)
-            return
-        }
+        if (!this.xInRange(x) || !this.yInRange(y)) return
         this.items[x + y * this.cols] = value
+    }
+
+    xInRange(x: number) {
+        return x > -1 && x < this.cols
+    }
+
+    yInRange(y: number) {
+        return y > -1 && y < this.rows
     }
 }
 
@@ -99,8 +100,7 @@ class Walker {
         }
 
         if (tries >= this.maxTries) {
-            newCells()
-            return
+            return newLayer()
         }
 
         cells.set(this.x, this.y, [this.scaleX, this.scaleY])
@@ -122,30 +122,7 @@ let cellsData = {
     iteration: -1,
 }
 
-function reset() {
-    ctx.clearRect(0, 0, width, height)
-    ctx.fillStyle = '#ffdebd'
-    ctx.fillRect(0, 0, width, height)
-
-    const { size, margin } = cellsData
-    let w = Math.min(width * 0.9, 1200)
-    let h = Math.min(height * 0.9, 1200)
-    const cols = Math.floor(w / (size + margin))
-    const rows = Math.floor(h / (size + margin))
-
-    const extraX = width - (size + margin) * cols + margin
-    const extraY = height - (size + margin) * rows + margin
-
-    cellsData.cols = cols
-    cellsData.rows = rows
-    cellsData.translateX = extraX / 2
-    cellsData.translateY = extraY / 2
-
-    newCells()
-    draw(true)
-}
-
-function newCells() {
+function newLayer() {
     cells = new Grid(cellsData.cols, cellsData.rows)
 
     cellsData.iteration++
@@ -158,27 +135,41 @@ function newCells() {
     walker.scaleY = random([1, -1])
 }
 
-function draw(base = false) {
-    let { size, margin, cols, rows } = cellsData
+function tile(cx: number, cy: number, size: number, flipX: 1 | -1 = 1, flipY: 1 | -1 = 1) {
+    let radius = size * 0.75
+    ctx.save()
+    ctx.translate(cx, cy)
+    ctx.scale(flipX, flipY)
+
+    ctx.beginPath()
+    ctx.moveTo(-size / 2, -size / 2)
+    ctx.arc(-size / 2, -size / 2, radius, 0, Math.PI * 0.5)
+    ctx.fill()
+
+    ctx.restore()
+}
+
+function draw(first = false) {
+    let { size, margin, cols, rows, translateX, translateY, bg, fg } = cellsData
 
     ctx.save()
-    ctx.translate(cellsData.translateX, cellsData.translateY)
+    ctx.translate(translateX, translateY)
 
     for (let i = 0; i < cols * rows; i++) {
         const x = i % cols
         const y = Math.floor(i / cols)
         let cell = cells.get(x, y)
 
-        if (cell || base) {
+        if (cell || first) {
             const cx = x * (size + margin) + size / 2
             const cy = y * (size + margin) + size / 2
 
             if (cell) {
                 let [scaleX, scaleY] = cell
-                ctx.fillStyle = cellsData.bg
+                ctx.fillStyle = bg
                 ctx.fillRect(cx - size / 2, cy - size / 2, size, size)
 
-                ctx.fillStyle = cellsData.fg
+                ctx.fillStyle = fg
                 tile(cx, cy, size, scaleX, scaleY)
             } else {
                 ctx.fillStyle = orange
@@ -192,19 +183,23 @@ function draw(base = false) {
     ctx.restore()
 }
 
-function tile(cx: number, cy: number, size: number, flipX: 1 | -1 = 1, flipY: 1 | -1 = 1) {
-    let radius = size * 0.75
-    ctx.save()
-    ctx.translate(cx, cy)
+function reset() {
+    ctx.fillStyle = '#ffdebd'
+    ctx.fillRect(0, 0, width, height)
 
-    ctx.scale(flipX, flipY)
+    const { size, margin } = cellsData
+    let w = Math.min(width * 0.9, 1200)
+    let h = Math.min(height * 0.9, 1200)
+    const cols = Math.floor(w / (size + margin))
+    const rows = Math.floor(h / (size + margin))
 
-    ctx.beginPath()
-    ctx.moveTo(-size / 2, -size / 2)
-    ctx.arc(-size / 2, -size / 2, radius, 0, Math.PI * 0.5)
-    ctx.fill()
+    cellsData.cols = cols
+    cellsData.rows = rows
+    cellsData.translateX = (width - (size + margin) * cols + margin) / 2
+    cellsData.translateY = (height - (size + margin) * rows + margin) / 2
 
-    ctx.restore()
+    newLayer()
+    draw(true)
 }
 
 window.addEventListener('resize', () => {
